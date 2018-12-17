c0f985c44ea4ec2b57e215f1b4b6e311cf6e56b1
FernandoBarrz <43194506+FernandoBarrz@users.noreply.github.com>
2018-12-17 12:04:16 -0600

    I made a new function and document the code
    I replaced the old code to make a better performance

--- prueba.js
+++ prueba.js
@@ -1,259 +1,215 @@
-
-//opePro(prompt("TE lo digo haci nomás"));
-
+/*
+ Función principal de la página
+ -Mediante la funcion "prompt" se obtiene un número decimal entre 0 - 100
+ -Se compara el número de entrada con la función.
+ -Mediante un "alert" se notifica el resultado en binario.
+*/
 function opePro(a){
 	if (a == 0) {
-		alert("0-Base 2 positiva: 0000 y Base 2 negativa: <-- 1111 1111 1111");
+		alert("0 en binario es: 0000");
 	}else if (a == 1) {
-		alert("1-Base 2 positiva: 0001 y Base 2 negativa: <-- 1111 1111 1110");
+		alert("1 en binario es: 0001");
 	}else if (a == 2) {
-		alert("2-Base 2 positiva: 0010 y Base 2 negativa: <-- 1111 1111 1101");
+		alert("2 en binario es: 0010");
 	}else if (a == 3) {
-		alert("3-Base 2 positiva: 0011 y Base 2 negativa: <-- 1111 1111 1100");
+		alert("3 en binario es: 0011");
 	}else if (a == 4) {
-		alert("4-Base 2 positiva: 0100 y Base 2 negativa: <-- 1111 1111 1011");
+		alert("4 en binario es: 0100");
 	}else if (a == 5) {
-		alert("5-Base 2 positiva: 0101 y Base 2 negativa: <-- 1111 1111 1010");
+		alert("5 en binario es: 0101");
 	}else if (a == 6) {
-		alert("6-Base 2 positiva: 0110 y Base 2 negativa: <-- 1111 1111 1001");
+		alert("6 en binario es: 0110");
 	}else if (a == 7) {
-		alert("7-Base 2 positiva: 0111 y Base 2 negativa: <-- 1111 1111 1000");
+		alert("7 en binario es: 0111");
 	}else if (a == 8) {
-		alert("8-Base 2 positiva: 1000 y Base 2 negativa: <-- 1111 1111 0111");
+		alert("8 en binario es: 1000");
 	}else if (a == 9) {
-		alert("9-Base 2 positiva: 1001 y Base 2 negativa: <-- 1111 1111 0110");
+		alert("9 en binario es: 1001");
 	}else if (a == 10) {
-		alert("10-Base 2 positiva: 1010 y Base 2 negativa: <-- 1111 1111 0101");
+		alert("10 en binario es: 1010");
 	}else if (a == 11) {
-		alert("11-Base 2 positiva: 1011 y Base 2 negativa: <-- 1111 1111 0100");
+		alert("11 en binario es: 1011");
 	}else if (a == 12) {
-		alert("12-Base 2 positiva: 1100 y Base 2 negativa: <-- 1111 1111 0011");
+		alert("12 en binario es: 1100");
 	}else if (a == 13) {
-		alert("13-Base 2 positiva: 1101 y Base 2 negativa: <-- 1111 1111 0010");
+		alert("13 en binario es: 1101");
 	}else if (a == 14) {
-		alert("14-Base 2 positiva: 1110 y Base 2 negativa: <-- 1111 1111 0001");
+		alert("14 en binario es: 1110");
 	}else if (a == 15) {
-		alert("15-Base 2 positiva: 1111 y Base 2 negativa: <-- 1111 1111 0000");
+		alert("15 en binario es: 1111");
 	}else if (a == 16) {
-		alert("16-Base 2 positiva: 0001-0000 y Base 2 negativa: <-- 1111 1110 1111");
+		alert("16 en binario es : 0001-0000 1111");
 	}else if (a == 17) {
-		alert("17-Base 2 positiva: 0001-0001 y Base 2 negativa: <-- 1111 1110 1110");
+		alert("17 en binario es : 0001-0001 1110");
 	}else if (a == 18) {
-		alert("18-Base 2 positiva: 0001-0010 y Base 2 negativa: <-- 1111 1110 1101");
+		alert("18 en binario es : 0001-0010 1101");
 	}else if (a == 19) {
-		alert("19-Base 2 positiva: 0001-0011 y Base 2 negativa: <-- 1111 1110 1100");
+		alert("19 en binario es: 0001-0011");
 	}else if (a == 20) {
-		alert("20-Base 2 positiva: 0001-0100 y Base 2 negativa: <-- 1111 1110 1011");
+		alert("20 en binario es: 0001-0100");
 	}else if (a == 21) {
-		alert("21-Base 2 positiva: 0001-0101 y Base 2 negativa: <-- 1111 1110 1010");
+		alert("21 en binario es: 0001-0101");
 	}else if (a == 22) {
-		alert("22-Base 2 positiva: 0001-0110 y Base 2 negativa: <-- 1111 1110 1001");
+		alert("22 en binario es: 0001-0110");
 	}else if (a == 23) {
-		alert("23-Base 2 positiva: 0001-0111 y Base 2 negativa: <-- 1111 1110 1000");
+		alert("23 en binario es: 0001-0111");
 	}else if (a == 24) {
-		alert("24-Base 2 positiva: 0001-1000 y Base 2 negativa: <-- 1111 1110 0111");
+		alert("24 en binario es: 0001-1000");
 	}else if (a == 25) {
-		alert("25-Base 2 positiva: 0001-1001 y Base 2 negativa: <-- 1111 1110 0110");
+		alert("25 en binario es: 0001-1001");
 	}else if (a == 26) {
-		alert("26-Base 2 positiva: 0001-1010 y Base 2 negativa: <-- 1111 1110 0101");
+		alert("26 en binario es: 0001-1010");
 	}else if (a == 27) {
-		alert("27-Base 2 positiva: 0001-0111 y Base 2 negativa: <-- 1111 1110 1000");
+		alert("27 en binario es: 0001-0111");
 	}else if (a == 28) {
-		alert("28-Base 2 positiva: 0001-1100 y Base 2 negativa: <-- 1111 1110 0011");
+		alert("28 en binario es: 0001-1100");
 	}else if (a == 29) {
-		alert("29-Base 2 positiva: 0001-1101 y Base 2 negativa: <-- 1111 1110 0010");
+		alert("29 en binario es: 0001-1101");
 	}else if (a == 30) {
-		alert("30-Base 2 positiva: 0001-1110 y Base 2 negativa: <-- 1111 1110 0001");
+		alert("30 en binario es: 0001-1110");
 	}else if (a == 31) {
-		alert("31-Base 2 positiva: 0001-1111 y Base 2 negativa: <-- 1111 1110 0000");
+		alert("31 en binario es: 0001-1111");
 	}else if (a == 32) {
-		alert("32-Base 2 positiva: 0010-0000 y Base 2 negativa: <-- 1111 1101 1111");
+		alert("32 en binario es: 0010-0000");
 	}else if (a == 33) {
-		alert("33-Base 2 positiva: 0010-0001 y Base 2 negativa: <-- 1111 1101 1110");
+		alert("33 en binario es: 0010-0001");
 	}else if (a == 34) {
-		alert("34-Base 2 positiva: 0010-0010 y Base 2 negativa: <-- 1111 1101 1101");
+		alert("34 en binario es: 0010-0010");
 	}else if (a == 35) {
-		alert("35-Base 2 positiva: 0010-0101 y Base 2 negativa: <-- 1111 1101 1010");
+		alert("35 en binario es: 0010-0101");
 	}else if (a == 36) {
-		alert("36-Base 2 positiva: 0010-0100 y Base 2 negativa: <-- 1111 1101 1011");
+		alert("36 en binario es: 0010-0100");
 	}else if (a == 37) {
-		alert("37-Base 2 positiva: 0010-0101 y Base 2 negativa: <-- 1111 1101 1010");
+		alert("37 en binario es: 0010-0101");
 	}else if (a == 38) {
-		alert("38-Base 2 positiva: 0010-0110 y Base 2 negativa: <-- 1111 1101 1001");
+		alert("38 en binario es: 0010-0110");
 	}else if (a == 39) {
-		alert("39-Base 2 positiva: 0010-0111 y Base 2 negativa: <-- 1111 1101 1000");
+		alert("39 en binario es: 0010-0111");
 	}else if (a == 40) {
-		alert("40-Base 2 positiva: 0010-1000 y Base 2 negativa: <-- 1111 1101 0111");
+		alert("40 en binario es: 0010-1000");
 	}else if (a == 41) {
-		alert("41-Base 2 positiva: 0010-1001 y Base 2 negativa: <-- 1111 1101 0110");
+		alert("41 en binario es: 0010-1001");
 	}else if (a == 42) {
-		alert("42-Base 2 positiva: 0010-1010 y Base 2 negativa: <-- 1111 1101 0101");
+		alert("42 en binario es: 0010-1010");
 	}else if (a == 43) {
-		alert("43-Base 2 positiva: 0010-1011 y Base 2 negativa: <-- 1111 1101 0100");
+		alert("43 en binario es: 0010-1011");
 	}else if (a == 44) {
-		alert("44-Base 2 positiva: 0010-1100 y Base 2 negativa: <-- 1111 1101 0011");
+		alert("44 en binario es: 0010-1100");
 	}else if (a == 45) {
-		alert("45-Base 2 positiva: 0010-1101 y Base 2 negativa: <-- 1111 1101 0010");
+		alert("45 en binario es: 0010-1101");
 	}else if (a == 46) {
-		alert("46-Base 2 positiva: 0010-1110 y Base 2 negativa: <-- 1111 1101 0001");
+		alert("46 en binario es: 0010-1110");
 	}else if (a == 47) {
-		alert("47-Base 2 positiva: 0010-1111 y Base 2 negativa: <-- 1111 1101 0000");
+		alert("47 en binario es: 0010-1111");
 	}else if (a == 48) {
-		alert("48-Base 2 positiva: 0011-0000 y Base 2 negativa: <-- 1111 1100 0000");
+		alert("48 en binario es: 0011-0000");
 	}else if (a == 49) {
-		alert("49-Base 2 positiva: 0011-0001 y Base 2 negativa: <-- 1111 1100 1110");
+		alert("49 en binario es: 0011-0001");
 	}else if (a == 50) {
-		alert("50-Base 2 positiva: 0011-0010 y Base 2 negativa: <-- 1111 1100 1101");
+		alert("50 en binario es: 0011-0010");
 	}else if (a == 51) {
-		alert("51-Base 2 positiva: 0011-0010 y Base 2 negativa: <-- 1111 1100 1101");
+		alert("51 en binario es: 0011-0010");
 	}else if (a == 52) {
-		alert("52-Base 2 positiva: 0011-0011 y Base 2 negativa: <-- 1111 1100 1100");
+		alert("52 en binario es: 0011-0011");
 	}else if (a == 53) {
-		alert("53-Base 2 positiva: 0011-0101 y Base 2 negativa: <-- 1111 1100 1010");
+		alert("53 en binario es: 0011-0101");
 	}else if (a == 54) {
-		alert("54-Base 2 positiva: 0011-0110 y Base 2 negativa: <-- 1111 1100 1001");
+		alert("54 en binario es: 0011-0110");
 	}else if (a == 55) {
-		alert("55-Base 2 positiva: 0011-0111 y Base 2 negativa: <-- 1111 1100 1000");
+		alert("55 en binario es: 0011-0111");
 	}else if (a == 56) {
-		alert("56-Base 2 positiva: 0011-1000 y Base 2 negativa: <-- 1111 1100 0111");
+		alert("56 en binario es: 0011-1000");
 	}else if (a == 57) {
-		alert("57-Base 2 positiva: 0011-1001 y Base 2 negativa: <-- 1111 1100 0110");
+		alert("57 en binario es: 0011-1001");
 	}else if (a == 58) {
-		alert("58-Base 2 positiva: 0011-1010 y Base 2 negativa: <-- 1111 1100 0101");
+		alert("58 en binario es: 0011-1010");
 	}else if (a == 59) {
-		alert("59-Base 2 positiva: 0011-1011 y Base 2 negativa: <-- 1111 1100 0100");
+		alert("59 en binario es: 0011-1011");
 	}else if (a == 60) {
-		alert("60-Base 2 positiva: 0011-1100 y Base 2 negativa: <-- 1111 1100 0011");
+		alert("60 en binario es: 0011-1100");
 	}else if (a == 61) {
-		alert("61-Base 2 positiva: 0011-1101 y Base 2 negativa: <-- 1111 1100 0010");
+		alert("61 en binario es: 0011-1101");
 	}else if (a == 62) {
-		alert("62-Base 2 positiva: 0011-1110 y Base 2 negativa: <-- 1111 1100 0001");
+		alert("62 en binario es: 0011-1110");
 	}else if (a == 63) {
-		alert("63-Base 2 positiva: 0011-1111 y Base 2 negativa: <-- 1111 1100 0000");
+		alert("63 en binario es: 0011-1111");
 	}else if (a == 64) {
-		alert("64-Base 2 positiva: 0100-0000 y Base 2 negativa: <-- 1111 1011 1111");
+		alert("64 en binario es: 0100-0000");
 	}else if (a == 65) {
-		alert("65-Base 2 positiva: 0100-0001 y Base 2 negativa: <-- 1111 1011 1110");
+		alert("65 en binario es: 0100-0001");
 	}else if (a == 66) {
-		alert("66-Base 2 positiva: 0100-0010 y Base 2 negativa: <-- 1111 1011 1101");
+		alert("66 en binario es: 0100-0010");
 	}else if (a == 67) {
-		alert("67-Base 2 positiva: 0100-0011 y Base 2 negativa: <-- 1111 1100 1100");
+		alert("67 en binario es: 0100-0011");
 	}else if (a == 68) {
-		alert("68-Base 2 positiva: 0100-0100 y Base 2 negativa: <-- 1111 1011 1011");
+		alert("68 en binario es: 0100-0100");
 	}else if (a == 69) {
-		alert("69-Base 2 positiva: 0100-0101 y Base 2 negativa: <-- 1111 1100 1010");
+		alert("69 en binario es: 0100-0101");
 	}else if (a == 70) {
-		alert("70-Base 2 positiva: 0100-0110 y Base 2 negativa: <-- 1111 1011 1001");
+		alert("70 en binario es: 0100-0110");
 	}else if (a == 71) {
-		alert("71-Base 2 positiva: 0100-0111 y Base 2 negativa: <-- 1111 1011 1000");
+		alert("71 en binario es: 0100-0111");
 	}else if (a == 72) {
-		alert("72-Base 2 positiva: 0100-1000 y Base 2 negativa: <-- 1111 1011 0111");
+		alert("72 en binario es: 0100-1000");
 	}else if (a == 73) {
-		alert("73-Base 2 positiva: 0100-1001 y Base 2 negativa: <-- 1111 1011 0110");
+		alert("73 en binario es: 0100-1001");
 	}else if (a == 74) {
-		alert("74-Base 2 positiva: 0100-1010 y Base 2 negativa: <-- 1111 1011 0101");
+		alert("74 en binario es: 0100-1010");
 	}else if (a == 75) {
-		alert("75-Base 2 positiva: 0100-1011 y Base 2 negativa: <-- 1111 1011 0100");
+		alert("75 en binario es: 0100-1011");
 	}else if (a == 76) {
-		alert("76-Base 2 positiva: 0100-1100 y Base 2 negativa: <-- 1111 1011 0011");
+		alert("76 en binario es: 0100-1100");
 	}else if (a == 77) {
-		alert("77-Base 2 positiva: 0100-1101 y Base 2 negativa: <-- 1111 1011 0010");
+		alert("77 en binario es: 0100-1101");
 	}else if (a == 78) {
-		alert("78-Base 2 positiva: 0100-1110 y Base 2 negativa: <-- 1111 1011 0001");
+		alert("78 en binario es: 0100-1110");
 	}else if (a == 79) {
-		alert("79-Base 2 positiva: 0100-1111 y Base 2 negativa: <-- 1111 1011 0000");
+		alert("79 en binario es: 0100-1111");
 	}else if (a == 80) {
-		alert("80-Base 2 positiva: 0101-0000 y Base 2 negativa: <-- 1111 1010 1111");
+		alert("80 en binario es: 0101-0000");
 	}else if (a == 81) {
-		alert("81-Base 2 positiva: 0101-0001 y Base 2 negativa: <-- 1111 1010 1110");
+		alert("81 en binario es: 0101-0001");
 	}else if (a == 82) {
-		alert("82-Base 2 positiva: 0101-0010 y Base 2 negativa: <-- 1111 1010 1101");
+		alert("82 en binario es: 0101-0010");
 	}else if (a == 83) {
-		alert("83-Base 2 positiva: 0101-0011 y Base 2 negativa: <-- 1111 1010 1100");
+		alert("83 en binario es: 0101-0011");
 	}else if (a == 84) {
-		alert("84-Base 2 positiva: 0101-0100 y Base 2 negativa: <-- 1111 1010 1011");
+		alert("84 en binario es: 0101-0100");
 	}else if (a == 85) {
-		alert("85-Base 2 positiva: 0101-0101 y Base 2 negativa: <-- 1111 1010 1010");
+		alert("85 en binario es: 0101-0101");
 	}else if (a == 86) {
-		alert("86-Base 2 positiva: 0101-0110 y Base 2 negativa: <-- 1111 1010 1001");
+		alert("86 en binario es: 0101-0110");
 	}else if (a == 87) {
-		alert("87-Base 2 positiva: 0101-0111 y Base 2 negativa: <-- 1111 1010 1000");
+		alert("87 en binario es: 0101-0111");
 	}else if (a == 88) {
-		alert("88-Base 2 positiva: 0101-1000 y Base 2 negativa: <-- 1111 1010 0111");
+		alert("88 en binario es: 0101-1000");
 	}else if (a == 89) {
-		alert("89-Base 2 positiva: 0101-1001 y Base 2 negativa: <-- 1111 1010 0110");
+		alert("89 en binario es: 0101-1001");
 	}else if (a == 90) {
-		alert("90-Base 2 positiva: 0101-1010 y Base 2 negativa: <-- 1111 1010 0101");
+		alert("90 en binario es: 0101-1010");
 	}else if (a == 91) {
-		alert("91-Base 2 positiva: 0101-1011 y Base 2 negativa: <-- 1111 1010 0100");
+		alert("91 en binario es: 0101-1011");
 	}else if (a == 92) {
-		alert("92-Base 2 positiva: 0101-1100 y Base 2 negativa: <-- 1111 1010 0011");
+		alert("92 en binario es: 0101-1100");
 	}else if (a == 93) {
-		alert("93-Base 2 positiva: 0101-1101 y Base 2 negativa: <-- 1111 1010 0010");
+		alert("93 en binario es: 0101-1101");
 	}else if (a == 94) {
-		alert("94-Base 2 positiva: 0101-1110 y Base 2 negativa: <-- 1111 1010 0001");
+		alert("94 en binario es: 0101-1110");
 	}else if (a == 95) {
-		alert("95-Base 2 positiva: 0101-1111 y Base 2 negativa: <-- 1111 1010 0000");
+		alert("95 en binario es: 0101-1111");
 	}else if (a == 96) {
-		alert("96-Base 2 positiva: 0110-0000 y Base 2 negativa: <-- 1111 1001 1111");
+		alert("96 en binario es: 0110-0000");
 	}else if (a == 97) {
-		alert("97-Base 2 positiva: 0110-0001 y Base 2 negativa: <-- 1111 1001 1110");
+		alert("97 en binario es: 0110-0001");
 	}else if (a == 98) {
-		alert("98-Base 2 positiva: 0110-0010 y Base 2 negativa: <-- 1111 1001 1101");
+		alert("98 en binario es: 0110-0010");
 	}else if (a == 99) {
-		alert("99-Base 2 positiva: 0110-0011 y Base 2 negativa: <-- 1111 1001 1100");
+		alert("99 en binario es: 0110-0011");
 	}else if (a == 100) {
-		alert("100-Base 2 positiva: 0110-0100 y Base 2 negativa: <-- 1111 1001 1011");
+		alert("100 en binario es: 0110-0100");
 	}else if (a > 100) {
 		alert("Solo números decimales entre 0 y 100");
 	}else if (a < 0) {
 		alert("Solo números decimales POSITIVOS entre 0 y 100");
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-//function miOp2(){
-//	if (prompt("Ingresa número base 10") == 0) {
-//		alert("Base 2 = 0000");
-//
-//
-//
-//
-//
-//
-//		
-//	}else if ( prompt() ===  1){
-//		alert("Base 2 = 0001");
-//	}
-//}
-//
-
-
-
-
-
-
-
-
-
-	//function miOp (){
-	//	prompt("Datos de entrada");
-	//	if (prompt == 11) {
-	//	console.log("El resultado es 10");
-	//	}else{
-	//		console.log('El resultado no es 10');
-	//	}
-	//}
-//miOp(alert('El resultado es 10'));
